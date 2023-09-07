97044d5035f9331a7820d629e5e3afbc1062cc33
Antonio Seraus <41804476+SerausAntonio@users.noreply.github.com>
2023-09-07 20:10:55 +0200

    Chrome headed

--- src/test/java/com/selenium/tests/base/DriverFactory.java
+++ src/test/java/com/selenium/tests/base/DriverFactory.java
@@ -8,7 +8,7 @@ public class DriverFactory {
     WebDriver driver;
     public WebDriver initializeDriver(){
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless", "--window-size=1920,1200");
+        options.addArguments("--headed", "--window-size=1920,1200");
         driver = new ChromeDriver(options);
         return driver;
     }
