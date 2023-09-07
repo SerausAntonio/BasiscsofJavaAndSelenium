64ced0b081d83e544f042febed9418dfebf536d2
Antonio Seraus <41804476+SerausAntonio@users.noreply.github.com>
2023-09-07 20:45:31 +0200

    Merge remote-tracking branch 'origin/master'

--- src/test/java/com/selenium/tests/base/DriverFactory.java
+++ src/test/java/com/selenium/tests/base/DriverFactory.java
@@ -8,7 +8,7 @@ public class DriverFactory {
     WebDriver driver;
     public WebDriver initializeDriver(){
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headed", "--window-size=1920,1200");
+        options.addArguments("--qqheaded", "--window-size=1920,1200");
         driver = new ChromeDriver(options);
         return driver;
     }
